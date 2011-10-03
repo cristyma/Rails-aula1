bdb093113a61e21b29ad0d64b86f559e78b89b99
studio1 <aluno@ubuntu.ubuntu-domain>
2011-10-03 11:57:31 -0300

    cria banco no rails

---
+++ aula02/agenda/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agenda::Application.config.secret_token = '4f3c202ce3f86b3f842c9d504a5c068cb794bea845582144548dab562c887d9bf3e1815d96fab2fa0e1953076cc8caaa7701d4884ec5dda83829b6e2be387376'
